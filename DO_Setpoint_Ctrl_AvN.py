0fe585219699a7c3836cd2e4ed2b87ac89a81a80
Niels Nicolaï <niels.nicolai@ugent.be>
2019-10-03 11:25:51 -0400

    Update D action accordig to book Visioli
    D = K*Td*s/((Td/N)*s+1)

--- DO_Setpoint_Ctrl_AvN.py
+++ DO_Setpoint_Ctrl_AvN.py
@@ -212,8 +212,8 @@ int_coeff_2 = h/Tt
 #dif_coeff_1 = (2*Td-N*h)/(2*Td+N*h)
 #dif_coeff_2 = 2*K*N*Td/(2*Td+N*h)
 #According to scilab:
-dif_coeff_1 = 1/(1+h*N) #scilab
-dif_coeff_2 = -Td*K*N/(1+N*h)
+dif_coeff_1 = Td/(Td+h*N) #scilab
+dif_coeff_2 = -Td*K*N/(Td+N*h)
 
 #Control action calculation
 PID_P = K*error
